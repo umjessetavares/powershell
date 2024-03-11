49db91c496820fe4a0b5b1ca36905091192c6f40
Jesse Tavares <jessetavarescardoso@gmail.com>
2024-03-11 14:08:54 -0300

    Update ipv4_dns_configuraton.ps1

--- ipv4_dns_configuraton.ps1
+++ ipv4_dns_configuraton.ps1
@@ -1,14 +1,18 @@
 # SCRIPT TO SET THE DNS ADDRESS OF THE ACTIVE INTERFACE
-# EXECUTE AS ADMINISTRATOR
-
+# ADMINISTRATOR PRIVILEGIES
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Este script requer privilégios de administrador. Reiniciando com privilégios de administrador..."
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ("-File", $MyInvocation.MyCommand.Path)
+    Exit
+}
 # New DNS Address
 $DNS1 = "1.1.1.3"
 $DNS2 = "1.0.0.3"
 
-# Current network interface
+# GET NETWORK INTERFACES
 $interface = Get-NetAdapter | Where-Object { $_.Status -eq "Up" }
 
-# Configuration
+# CONFIGURATION
 if ($interface) {
     $interface | Set-DnsClientServerAddress -ServerAddresses ($DNS1, $DNS2)
     Write-Host "DNS alterado para $DNS1 e $DNS2 com sucesso!"
